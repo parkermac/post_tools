a4369e0e99b878146ec1b063eb1e3494ad5a0820
lederer <lederer@b458f3c2-887a-46d4-808d-831d0bf0c359>
2010-01-07 19:03:15 +0000

    make_movie code now does not eat up an offensive amount of memory to do a simple task.

--- rompy/make_movie.py
+++ rompy/make_movie.py
@@ -14,32 +14,62 @@ from optparse import OptionParser
 from AppKit import NSImage
 from QTKit import QTMovie, QTMakeTime, QTAddImageCodecType
 
+def movie_maker(movdir,imgdir,timeval,timescale,ih):
+	print('making movie %s' % ih)
+	imgpaths = glob.glob(os.path.join(imgdir,'ocean_his_*_%s.png'%ih))
+	imgpaths.sort()
+	
+	movpath = os.path.join(movdir,'%s.mov' %ih )
+	
+	'''I am creating a new image sequence (QuickTime movie).'''
+	mov, err = QTMovie.alloc().initToWritableFile_error_(movpath, None)
+	
+	if mov == None:
+		errmsg = 'Could not create movie file: %s' % (movpath)
+		raise IOError, errmsg
+		
+	duration = QTMakeTime(timeval, timescale)
+	# you can also use "tiff"
+#		attrs = {QTAddImageCodecType: 'avc1'}#, QTAddImageCodecQuality: codecHighQuality}
+	attrs = {QTAddImageCodecType: 'avc1'}
+	
+	for imgpath in imgpaths:
+#		img = NSImage.alloc().initWithContentsOfFile_(imgpath)
+		img = NSImage.alloc().initByReferencingFile_(imgpath)
+		mov.addImage_forDuration_withAttributes_(img, duration, attrs)
+		del(img)
+	print('Writing movie %s' %ih)
+	mov.updateMovieFile()
+#	del(mov, err, attrs,imgpaths, duration)
+
 def crtimagesequence(movdir, imgdir, timeval, timescale):
 	img_handles = ['hood_salt','hood_temp','hood_U','main_salt','main_temp','main_U','surface_salt','surface_temp']
+#	img_handles = ['main_salt','main_temp','main_U','surface_salt','surface_temp']
 #	img_handles = ['hood_salt']
 	for ih in img_handles:
-		print('making movie %s' % ih)
-		imgpaths = glob.glob(os.path.join(imgdir,'ocean_his_*_%s.png'%ih))
-		imgpaths.sort()
-		
-		movpath = os.path.join(movdir,'%s.mov' %ih )
-		
-		'''I am creating a new image sequence (QuickTime movie).'''
-		mov, err = QTMovie.alloc().initToWritableFile_error_(movpath, None)
-		
-		if mov == None:
-			errmsg = 'Could not create movie file: %s' % (movpath)
-			raise IOError, errmsg
-			
-		duration = QTMakeTime(timeval, timescale)
-		# you can also use "tiff"
-#		attrs = {QTAddImageCodecType: 'avc1'}#, QTAddImageCodecQuality: codecHighQuality}
-		attrs = {QTAddImageCodecType: 'avc1'}
-		
-		for imgpath in imgpaths:
-			img = NSImage.alloc().initWithContentsOfFile_(imgpath)
-			mov.addImage_forDuration_withAttributes_(img, duration, attrs)
-		mov.updateMovieFile()
+		movie_maker(movdir,imgdir,timeval,timescale,ih)
+# 		print('making movie %s' % ih)
+# 		imgpaths = glob.glob(os.path.join(imgdir,'ocean_his_*_%s.png'%ih))
+# 		imgpaths.sort()
+# 		
+# 		movpath = os.path.join(movdir,'%s.mov' %ih )
+# 		
+# 		'''I am creating a new image sequence (QuickTime movie).'''
+# 		mov, err = QTMovie.alloc().initToWritableFile_error_(movpath, None)
+# 		
+# 		if mov == None:
+# 			errmsg = 'Could not create movie file: %s' % (movpath)
+# 			raise IOError, errmsg
+# 			
+# 		duration = QTMakeTime(timeval, timescale)
+# 		# you can also use "tiff"
+# #		attrs = {QTAddImageCodecType: 'avc1'}#, QTAddImageCodecQuality: codecHighQuality}
+# 		attrs = {QTAddImageCodecType: 'avc1'}
+# 		
+# 		for imgpath in imgpaths:
+# 			img = NSImage.alloc().initWithContentsOfFile_(imgpath)
+# 			mov.addImage_forDuration_withAttributes_(img, duration, attrs)
+# 		mov.updateMovieFile()
 
 if __name__ == '__main__':
 	# file path to the new QuickTime movie
@@ -53,3 +83,4 @@ if __name__ == '__main__':
 	timescale = float(sys.argv[4])
 	
 	crtimagesequence(movdir, imgdir, timeval, timescale)
+	
